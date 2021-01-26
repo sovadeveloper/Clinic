93af495ea3a8cc306ab24aa3843ae915ab3cfc06
sovadeveloper <sovadeveloper@mail.ru>
2021-01-26 17:09:20 +0300

    All tests complete

--- test/java/com/learning/ClinicTest.java
+++ test/java/com/learning/ClinicTest.java
@@ -3,25 +3,45 @@ package com.learning;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClinicTest {
     Clinic cl = new Clinic();
     String first = "First";
     String second = "Second";
+    String third = "Third";
 
     @Test
     void addClient() {
-        Client client = new Client(first);
-        Animal animal = new Animal(second);
-        cl.clients.add(client);
-        cl.pets.add(animal);
-        Assertions.assertEquals(client, cl.clients.get(0));
-        Assertions.assertEquals(animal, cl.pets.get(0));
+        cl.addClient(first, second);
+        Assertions.assertEquals(cl.clients.get(cl.clients.size() - 1).getClientName(), first);
+        Assertions.assertEquals(cl.pets.get(cl.pets.size() - 1).getPetName(), second);
     }
 
     @Test
     void deleteClient() {
+        int startSizeClients = cl.clients.size();
+        int startSizePets = cl.pets.size();
+        cl.addClient(first, second);
+        cl.deleteClient(first);
+        Assertions.assertEquals(startSizeClients, cl.clients.size());
+        Assertions.assertEquals(startSizePets, cl.pets.size());
+    }
 
+    @Test
+    void renameClient() {
+        cl.addClient(first, second);
+        cl.renameClient(first, third);
+        Assertions.assertEquals(third, cl.clients.get(cl.clients.size() - 1).getClientName());
+    }
+
+    @Test
+    void renameAnimal() {
+        cl.addClient(first, second);
+        cl.renameAnimal(second, third);
+        Assertions.assertEquals(third, cl.pets.get(cl.pets.size() - 1).getPetName());
     }
 }
